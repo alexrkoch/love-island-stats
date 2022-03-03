f54adf151916127b08d3450cb7c6a2b3d01cc987
Alex <alexrkoch@gmail.com>
2022-03-03 10:08:04 -0500

    clean up, final site files

--- wp-content/themes/_wp-bootstrap-starter/love-island-forms/input-actions.php
+++ wp-content/themes/_wp-bootstrap-starter/love-island-forms/input-actions.php
@@ -7,14 +7,6 @@ get_header();
 // $episode = FIGURE OUT HOW TO PULL FROM THE DB
 
 ?>
-<!-- FORM TO ADD A NEW ISLANDER -->
-<!-- <form action='' method='post'>
-  <label for="islander-name">New Islander's Name
-    <input type="text" name="islander-name" id="islander-name" value=''>
-  </label>
-  <input type="submit" name="new-islander" value="Add Islander">
-</form> -->
-
 
 <?php 
   global $wpdb;
@@ -48,21 +40,19 @@ if (isset($_POST['submit-action'])){
   $table_name = $wpdb->prefix.'islander_actions';
   $result = $wpdb->insert($table_name, $data);
 
+  // display if data was saved properly or not
   if ($result==1) {
-    echo "<h3> Data Saved </h3>";
+    echo "<div class='col-12 row justify-content-center'><h2 class='success-message'> ✅ {$islanderActing} + {$action} + ${withIslander} ✅  </h2></div>";
   } else{
-    echo "<h3> DATA NOT SAVED </h3>";
+    echo "<h2> DATA NOT SAVED </h3>";
   }
 
+  // Update the last entries so they pre-populate in the form correctly.
   $lastSeries = $wpdb->get_var( 'SELECT series FROM `wp_islander_actions` WHERE id=(SELECT MAX(id) FROM `wp_islander_actions`)' , 0, 0 ); 
   $lastSeason = $wpdb->get_var( 'SELECT season FROM `wp_islander_actions` WHERE id=(SELECT MAX(id) FROM `wp_islander_actions`)' , 0, 0 ); 
   $lastEpisode = $wpdb->get_var( 'SELECT episode FROM `wp_islander_actions` WHERE id=(SELECT MAX(id) FROM `wp_islander_actions`)' , 0, 0 ); 
   $boy_islanders = $wpdb->get_col( 'SELECT islander_name FROM `wp_current_boy_islanders`'); 
   $girl_islanders = $wpdb->get_col( 'SELECT islander_name FROM `wp_current_girl_islanders`'); 
-
-  // after submitting this form, redirect the user to the same page safely
-  // $location = "http://" . $_SERVER['HTTP_POST'] . $_SERVER['REQUEST_URI'] ;
-  // wp_safe_redirect($location);
 }
 ?>
 
@@ -70,10 +60,10 @@ if (isset($_POST['submit-action'])){
 <form action='' method='post' class="container-fluid">
   <div class="row">
     <div class="left-column col">
-      <div class="islander-acting row border p-1">
-        <div class="islander-acting-girls col btn-group btn-group-toggle" data-toggle="buttons">
+      <div class="islander-acting row border p-1 btn-group-vertical btn-group-toggle" data-toggle="buttons">
+        <div class="islander-acting-girls col " >
           <?php foreach ($girl_islanders as $girl_islander): ?> 
-            <label class="btn btn-lg btn-girl-islander mb-1" for="<?= $girl_islander; ?>">              
+            <label class="btn btn-lg btn-girl-islander mb-1 w-100" for="<?= $girl_islander; ?>">              
               <input type="radio" name="islander-acting" id="<?= $girl_islander; ?>" value='<?= $girl_islander; ?>' autocomplete="off">
               <?= $girl_islander; ?>
             </label> 
@@ -81,10 +71,10 @@ if (isset($_POST['submit-action'])){
         </div>
         <div class="islander-acting-boys col">
           <?php foreach ($boy_islanders as $boy_islander): ?> 
-            <div class="islander-option"> 
-              <input type="radio" name="islander-acting" id="<?= $boy_islander; ?>" value='<?= $boy_islander; ?>'>
-              <label for="<?= $boy_islander; ?>"><?= $boy_islander; ?></label> 
-            </div>
+            <label class="btn btn-lg btn-boy-islander mb-1 w-100" for="<?= $boy_islander; ?>">              
+              <input type="radio" name="islander-acting" id="<?= $boy_islander; ?>" value='<?= $boy_islander; ?>' autocomplete="off">
+              <?= $boy_islander; ?>
+            </label> 
           <?php endforeach; ?>
         </div>
       </div>
@@ -104,81 +94,80 @@ if (isset($_POST['submit-action'])){
       </div>
     </div>
     <div class="middle-column col">      
-      <div class="islander-actions row border p-1 ml-1 mr-1">
+      <div class="islander-actions row border p-1 ml-1 mr-1 col btn-group-vertical btn-group-toggle" data-toggle="buttons">
         <div class="islander-actions-column-left col">
-          <div class="islander-action">
-            <input type="radio" name="action" id="melt" value='Melt'>
-            <label for="melt">Melt</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="ick" value='Ick'>
-            <label for="ick">Ick</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="peck" value='Peck'>
-            <label for="peck">Peck</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="snog" value='Snog'>
-            <label for="snog">Snog</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="start-conflict" value='Start Conflict'>
-            <label for="start-conflict">StartCon</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="participate-conflict" value='Participate Conflict'>
-            <label for="participate-conflict">PartCon</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="bfgf" value='BFGF'>
-            <label for="bfgf">BFGF</label>        
-          </div>
+          <label for="melt" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="melt" value='Melt'>Melt
+          </label>
+          <label for="snog" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="snog" value='Snog'>Snog
+          </label>
+          <label for="chat-pull" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="chat-pull" value='Chat Pull'>Chat Pull
+          </label>  
+          <label for="start-conflict" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="start-conflict" value='Start Conflict'>StartCon
+          </label>
+          <label for="date" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="date" value='Date'>Date
+          </label>
+          <label for="bfgf" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="bfgf" value='BFGF'>BFGF
+          </label>
+          <label for="ily" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="ily" value='ILY'>ILY
+          </label>  
+          <label for="enter" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="enter" value='Enter'>Enter
+          </label>      
+           <label for="couple" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="couple" value='Couple'>Couple
+          </label> 
         </div>
         <div class="islander-actions-column-right col">
-          <div class="islander-action">
-            <input type="radio" name="action" id="breakup" value='Breakup'>
-            <label for="breakup">Breakup</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="date" value='Date'>
-            <label for="date">Date</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="dbs" value='DBS'>
-            <label for="dbs">DBS</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="text-message" value='Text Message'>
-            <label for="text-message">Text</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="chat-pull" value='Chat Pull'>
-            <label for="chat-pull">Chat</label>        
-          </div>
-          <div class="islander-action">
-            <input type="radio" name="action" id="hideaway" value='Hideaway'>
-            <label for="hideaway">Hideaway</label>        
-          </div>
+          <label for="ick" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="ick" value='Ick'>Ick
+          </label>   
+          <label for="peck" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="peck" value='Peck'>Peck
+          </label>   
+          <label for="text-message" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="text-message" value='Text Message'>Text
+          </label>  
+          <label for="participate-conflict" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="participate-conflict" value='Participate Conflict'>PartCon
+          </label>  
+          <label for="hideaway" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="hideaway" value='Hideaway'>Hideaway
+          </label>  
+          <label for="breakup" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="breakup" value='Breakup'>Breakup
+          </label>  
+          <label for="dbs" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="dbs" value='DBS'>DBS
+          </label>  
+          <label for="exit" class="btn btn-lg btn-islander-action mb-1 w-100">
+            <input type="radio" name="action" id="exit" value='Exit'>Exit
+          </label>      
         </div>        
       </div>
     </div>  
     <div class="right-column col"> 
-      <div class="islander-with row border p-1">
-        <div class="islander-with-girls col">
+      <div class="islander-acting row border p-1 btn-group-vertical btn-group-toggle" data-toggle="buttons">
+        <div class="islander-acting-girls col " >
           <?php foreach ($girl_islanders as $girl_islander): ?> 
-            <div class="islander-option"> 
-              <input type="radio" name="with-islander" id="<?= "with-".$girl_islander; ?>" value='<?= $girl_islander; ?>'>
-              <label for="<?= "with-".$girl_islander; ?>"><?= $girl_islander; ?></label> 
-            </div>
+            <label class="btn btn-lg btn-girl-islander mb-1 w-100" for="<?=  "with-".$girl_islander; ?>">              
+              <input type="radio" name="with-islander" id="<?= "with-".$girl_islander; ?>" value='<?= $girl_islander; ?>' autocomplete="off">
+              <?= $girl_islander; ?>
+            </label> 
           <?php endforeach; ?>
         </div>
-        <div class="islander-with-boys col">
+        <div class="islander-acting-boys col">
           <?php foreach ($boy_islanders as $boy_islander): ?> 
-            <div class="islander-option"> 
-              <input type="radio" name="with-islander" id="<?= "with-".$boy_islander; ?>" value='<?= $boy_islander; ?>'>
-              <label for="<?= "with-".$boy_islander; ?>"><?= $boy_islander; ?></label> 
-            </div>
+            <label class="btn btn-lg btn-boy-islander mb-1 w-100" for="<?= "with-".$boy_islander; ?>">              
+              <input type="radio" name="with-islander" id="<?= "with-".$boy_islander; ?>" value='<?= $boy_islander; ?>' autocomplete="off">
+              <?= $boy_islander; ?>
+            </label> 
           <?php endforeach; ?>
         </div>
       </div>
@@ -187,15 +176,13 @@ if (isset($_POST['submit-action'])){
           <label for="notes">Notes</label>    
           <input type="text" name="notes" id="notes" value='' class="col-12"> 
         </div>
-        <div class="submit-button p-1">
+        <div class="submit-button p-1 col-12">
           <input type="submit" name="submit-action" value="Submit Action" class="btn-lg col-12"> 
         </div>
       </div>
     </div>      
   </div>
 </form>
-
-
 
 <script>
     if ( window.history.replaceState ) {
--- wp-content/themes/_wp-bootstrap-starter/love-island-forms/modify-islanders.php
+++ wp-content/themes/_wp-bootstrap-starter/love-island-forms/modify-islanders.php
@@ -1,200 +1,71 @@
 <?php
-  // Template Name: Modify Islanders
+  // Template Name: Edit Islanders
 ob_start();
 get_header();
 
-// establish a variable for episode number
-// $episode = FIGURE OUT HOW TO PULL FROM THE DB
 
 ?>
-<!-- FORM TO ADD A NEW ISLANDER -->
-<!-- <form action='' method='post'>
-  <label for="islander-name">New Islander's Name
-    <input type="text" name="islander-name" id="islander-name" value=''>
-  </label>
-  <input type="submit" name="new-islander" value="Add Islander">
-</form> -->
-
-<!-- FUNCTION TO ADD NEW ISLANDER TO RADIO OPTIONS -->
-<!-- <?php
-// if (isset($_POST['new-islander'])){
-//   $name = (! empty($_POST['islander-name'])) ? sanitize_text_field($_POST['islander-name']) : '' ;
-//   $data = array(
-//     'firstName' => $name
-//   );
-//   $table_name = $wpdb->prefix.'islanders';
-//   $result = $wpdb->insert($table_name, $data);
-//   if ($result==1) {
-//     echo "<script>alert('Islander Data Saved');</script>";
-//   } else{
-//       echo "<script>alert('Unable to Save');</script>";
-//   }
-
-//   // after submitting this form, redirect the user to the same page safely
-//   // $location = "http://" . $_SERVER['HTTP_POST'] . $_SERVER['REQUEST_URI'] ;
-//   // wp_safe_redirect($location);
-// }
-?> -->
-
-
-<?php 
-  global $wpdb;
-  $lastEpisode = $wpdb->get_var( 'SELECT MAX(episode) FROM `wp_islander_actions`', 0, 0 ); 
-  $boy_islanders = $wpdb->get_col( 'SELECT islander_name FROM `wp_current_boy_islanders`'); 
-  $girl_islanders = $wpdb->get_col( 'SELECT islander_name FROM `wp_current_girl_islanders`');  
-  ?>
-  
-  
-    <form action='' method='post' class="container-fluid">
-      <div class="row p-1">
-        <label for="episdoe">Episode</label>        
-        <input type="text" name="episode" id="episode" value='<?php echo $lastEpisode ?>' class="col-2"> 
-      </div>
-
-      <div class="container row border p-1">
-        <div class="islander-acting-girls col">
-          <?php foreach ($girl_islanders as $girl_islander): ?> 
-            <div class="islander-option"> 
-              <input type="radio" name="islander-acting" id="<?= $girl_islander; ?>" value='<?= $girl_islander; ?>'>
-              <label for="<?= $girl_islander; ?>"><?= $girl_islander; ?></label> 
-            </div>
-          <?php endforeach; ?>
-        </div>
-        <div class="islander-acting-boys col">
-          <?php foreach ($boy_islanders as $boy_islander): ?> 
-            <div class="islander-option"> 
-              <input type="radio" name="islander-acting" id="<?= $boy_islander; ?>" value='<?= $boy_islander; ?>'>
-              <label for="<?= $boy_islander; ?>"><?= $boy_islander; ?></label> 
-            </div>
-          <?php endforeach; ?>
-        </div>
-      </div>
-
-      <div class="container row border p-1">
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="melt" value='Melt'>
-            <label for="melt">Melt</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="ick" value='Ick'>
-            <label for="ick">Ick</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="peck" value='Peck'>
-            <label for="peck">Peck</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="snog" value='Snog'>
-            <label for="snog">Snog</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="start-conflict" value='Start Conflict'>
-            <label for="start-conflict">StartCon</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="participate-conflict" value='Participate Conflict'>
-            <label for="participate-conflict">PartCon</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="enter" value='Enter'>
-            <label for="enter">Enter</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="exit" value='Exit'>
-            <label for="exit">Exit</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="bfgf" value='BFGF'>
-            <label for="bfgf">BFGF</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="breakup" value='Breakup'>
-            <label for="breakup">Breakup</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="date" value='Date'>
-            <label for="date">Date</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="dbs" value='DBS'>
-            <label for="dbs">DBS</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="text-message" value='Text Message'>
-            <label for="text-message">Text</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="chat-pull" value='Chat Pull'>
-            <label for="chat-pull">Chat</label>        
-          </div>
-          <div class="islander-action m-2">
-            <input type="radio" name="action" id="hideaway" value='Hideaway'>
-            <label for="hideaway">Hideaway</label>        
-          </div>
-
-          
-      </div>
-
-      <div class="container row border p-1">
-        <div class="islander-with-girls col">
-          <?php foreach ($girl_islanders as $girl_islander): ?> 
-            <div class="islander-option"> 
-              <input type="radio" name="with-islander" id="<?= "with-".$girl_islander; ?>" value='<?= $girl_islander; ?>'>
-              <label for="<?= "with-".$girl_islander; ?>"><?= $girl_islander; ?></label> 
-            </div>
-          <?php endforeach; ?>
-        </div>
-        <div class="islander-with-boys col">
-          <?php foreach ($boy_islanders as $boy_islander): ?> 
-            <div class="islander-option"> 
-              <input type="radio" name="with-islander" id="<?= "with-".$boy_islander; ?>" value='<?= $boy_islander; ?>'>
-              <label for="<?= "with-".$boy_islander; ?>"><?= $boy_islander; ?></label> 
-            </div>
-          <?php endforeach; ?>
-        </div>
-      </div>
-
-      <div class="row p-1">
-        <label for="notes">Notes</label>    
-        <input type="text" name="notes" id="notes" value='' class="col-12"> 
-      </div>
-
-      <div class="row p-1">
-        
-        <input type="submit" name="submit-action" value="Submit Action" class="btn-lg col-12"> 
-      </div>
-    </form>
 
 <?php
-if (isset($_POST['submit-action'])){
+if (isset($_POST['submit-edit-islanders'])){
   global $wpdb;
-  $episode = (! empty($_POST['episode'])) ? $_POST['episode'] : '' ;
-  $islanderActing = (! empty($_POST['islander-acting'])) ? $_POST['islander-acting'] : '' ;
-  $action = (! empty($_POST['action'])) ? $_POST['action'] : '' ;
-  $withIslander = (! empty($_POST['with-islander'])) ? $_POST['with-islander'] : '' ;
-  $notes = (! empty($_POST['notes'])) ? $_POST['notes'] : '' ;
+  $islanderTraveling = (! empty($_POST['edit-islanders'])) ? $_POST['edit-islanders'] : '' ;
+  $islanderGender = (! empty($_POST['islander-gender'])) ? $_POST['islander-gender'] : '' ;
+  $action = (! empty($_POST['islander-travel'])) ? $_POST['islander-travel'] : '' ;
 
-  $data = array(
-    'episode' => $episode,
-    'islanderActing' => $islanderActing,
-    'action' => $action,
-    'withIslander' => $withIslander,
-    'notes' => $notes,
-  );
-  $table_name = $wpdb->prefix.'islander-actions';
-  $result = $wpdb->insert($table_name, $data);
-
-  if ($result==1) {
-    echo "<h3> Data Saved </h3>";
-  } else{
-    echo "<h3> DATA NOT SAVED </h3>";
+  $islanderTravel = array(
+     'islander_name' => $islanderTraveling
+      );
+      
+  // decide which islander table to use
+  if ($islanderGender == "Girl") {
+    $islander_table_name = $wpdb->prefix.'current_girl_islanders';
+  } elseif ($islanderGender == "Boy") {
+    $islander_table_name = $wpdb->prefix.'current_boy_islanders';
+  }
+  
+  // decide if adding or deleting an islander
+  if ($action == "Enter") {
+    $islander_result = $wpdb->insert($islander_table_name, $islanderTravel);
+  } elseif ($action == "Exit") {
+    $islander_result = $wpdb->delete($islander_table_name, $islanderTravel);
   }
 
-  // after submitting this form, redirect the user to the same page safely
-  // $location = "http://" . $_SERVER['HTTP_POST'] . $_SERVER['REQUEST_URI'] ;
-  // wp_safe_redirect($location);
+  // display if data was saved properly or not
+  if ($islander_result==1) {
+    echo "<div class='col-12 row justify-content-center'><h2 class='success-message'> ✅ {$islanderTraveling} + {$action} ✅  </h2></div>";
+  } else {
+    echo "<h2> DATA NOT SAVED </h3>";
+  }
 }
 ?>
+
+
+<form class="" action='' method='post'>
+    <label class="p-1" for="edit-islanders">Islander</label>        
+    <input class="p-1" type="text" name="edit-islanders" id="edit-islanders" value="">
+    <div class="islander-acting column border p-1 btn-group btn-group-toggle" data-toggle="buttons">
+        <label class="btn btn-lg btn-girl-islander mb-1" for="islander-is-girl">
+            <input type="radio" name="islander-gender" id="islander-is-girl" value='Girl'>Girl
+        </label>
+        <label class="btn btn-lg btn-boy-islander mb-1" for="islander-is-boy">
+            <input type="radio" name="islander-gender" id="islander-is-boy" value='Boy'>Boy
+        </label>
+    </div>
+    <div class="islander-acting column border p-1 btn-group btn-group-toggle" data-toggle="buttons">
+        <label class="btn btn-lg btn-islander-action mb-1" for="islander-enter">
+            <input type="radio" name="islander-travel" id="islander-enter" value='Enter'>Enter
+        </label>
+        <label class="btn btn-lg btn-islander-action mb-1" for="islander-exit">
+            <input type="radio" name="islander-travel" id="islander-exit" value='Exit'>Exit
+        </label>
+    </div>
+    <input class="p-1 ml-1 btn-md submit-button" type="submit" name="submit-edit-islanders" value="Edit Islanders"> 
+</form>
+
+
+
+
 
 <script>
     if ( window.history.replaceState ) {
